5ce4c1ca6c69a5187ea9291f12f2c1d6075f3196
Greg Sleap <greg.sleap@curtin.edu.au>
2019-01-16 12:24:03 +0800

    Refactor to be more of a generic dir watcher and processor

--- mwax_sub2db.py
+++ mwax_sub2db.py
@@ -17,33 +17,54 @@ EVENT_TYPE_DELETED = 'deleted'
 EVENT_TYPE_CREATED = 'created'
 EVENT_TYPE_MODIFIED = 'modified'
 
-SUBFILE_EXTENSION = ".sub"
-FINISHED_EXTENSION = ".done"
+FILE_REPLACEMENT_TOKEN = "__FILE__"
 
-MODE_CORRELATOR = "CORRELATOR"
-MODE_OFFLINE_CORRELATOR = "OFFLINE_CORRELATOR"
-MODE_VOLTAGE_CAPTURE = "VOLTAGE_CAPTURE"
-
+MODE_WATCH_DIR_FOR_RENAME = "WATCH_DIR_FOR_RENAME"
+MODE_WATCH_DIR_FOR_NEW = "WATCH_DIR_FOR_NEW"
+MODE_PROCESS_DIR = "PROCESS_DIR"
 
 class RenameFileHandler(PatternMatchingEventHandler):
     def __init__(self, pattern, q, log):
         self.q = q
         self.logger = log
+        # this takes the pattern- e.g. *.sub and returns the last string after the "."
+        self.watch_ext = "." + pattern.split(".")[-1]
         PatternMatchingEventHandler.__init__(self, patterns=pattern)
 
     def on_moved(self, event):
         dest_filename = event.dest_path
 
-        if os.path.splitext(dest_filename)[1] == SUBFILE_EXTENSION:
+        if os.path.splitext(dest_filename)[1] == self.watch_ext:
+            self.q.put(dest_filename)
+            self.logger.info(f'Added {dest_filename} to queue')
+
+
+class NewFileHandler(PatternMatchingEventHandler):
+    def __init__(self, pattern, q, log):
+        self.q = q
+        self.logger = log
+        # this takes the pattern- e.g. *.sub and returns the last string after the "."
+        self.watch_ext = "." + pattern.split(".")[-1]
+        PatternMatchingEventHandler.__init__(self, patterns=pattern)
+
+    def on_created(self, event):
+        dest_filename = event.src_path
+
+        if os.path.splitext(dest_filename)[1] == self.watch_ext:
             self.q.put(dest_filename)
             self.logger.info(f'Added {dest_filename} to queue')
 
 
 class Watcher(object):
-    def __init__(self, path='.', recursive=True, q=None, pattern="*.*", log=None):
+    def __init__(self, path='.', recursive=True, q=None, pattern=None, log=None, mode=None):
         self.logger = log
         self._observer = Observer()
-        self._observer.schedule(RenameFileHandler(pattern, q, self.logger), path, recursive)
+        self.mode = mode
+
+        if self.mode == MODE_WATCH_DIR_FOR_RENAME:
+            self._observer.schedule(RenameFileHandler(pattern, q, self.logger), path, recursive)
+        elif self.mode == MODE_WATCH_DIR_FOR_NEW:
+            self._observer.schedule(NewFileHandler(pattern, q, self.logger), path, recursive)
 
     def start(self):
         self.logger.info("Watcher starting...")
@@ -57,13 +78,13 @@ class Watcher(object):
 
 
 class QueueWorker(object):
-    def __init__(self, q, dada_diskdb_path, dada_key, mode, log):
+    def __init__(self, q, executable_path, mode, log, rename_ext):
         self._q = q
-        self._dada_diskdb_path = dada_diskdb_path
-        self._dada_key = dada_key
+        self._executable_path = executable_path
         self._running = False
         self._mode = mode
         self.logger = log
+        self.rename_ext = rename_ext
 
     def start(self):
         self.logger.info("QueueWorker starting...")
@@ -77,7 +98,7 @@ class QueueWorker(object):
                 self._q.task_done()
                 self.logger.info(f"Processing {item} Complete... Queue size: {self._q.qsize()}")
             except queue.Empty:
-                if self._mode == MODE_OFFLINE_CORRELATOR:
+                if self._mode == MODE_PROCESS_DIR:
                     # Queue is complete. Stop now
                     self.logger.info("Finished processing queue.")
                     self.stop()
@@ -93,11 +114,15 @@ class QueueWorker(object):
             self.logger.info("QueueWorker stopping... finishing remaining items")
 
     def run_command(self, filename):
-        command = f"{self._dada_diskdb_path} -k {self._dada_key} -f {filename} -s"
+        command = f"{self._executable_path}"
+
+        # Substitute the filename into the command
+        command = command.replace(FILE_REPLACEMENT_TOKEN, filename)
+
         # Example: "dada_diskdb -k 1234 -f 1216447872_02_256_201.sub -s"
 
         new_filename_no_ext = os.path.splitext(filename)[0]
-        new_filename = os.path.join(os.path.dirname(filename), new_filename_no_ext + FINISHED_EXTENSION)
+        new_filename = os.path.join(os.path.dirname(filename), new_filename_no_ext + self.rename_ext)
 
         try:
             # Execute the command
@@ -109,6 +134,7 @@ class QueueWorker(object):
             if return_code == 0:
                 # Success - rename the file
                 try:
+                    self.logger.info(f"Renaming {filename} to {new_filename}")
                     os.rename(filename, new_filename)
                 except Exception as rename_exception:
                     self.logger.error(f"Error renaming {filename} to {new_filename}: {str(rename_exception)}")
@@ -129,8 +155,9 @@ class Processor:
 
         # init vars
         self.watch_dir = None
-        self.dada_key = None
-        self.dada_diskdb = None
+        self.watch_ext = None
+        self.rename_ext = None
+        self.executable = None
         self.mode = None
         self.q = None
         self.watch = None
@@ -155,39 +182,41 @@ class Processor:
 
         # Get command line args
         parser = argparse.ArgumentParser()
-        parser.description = "mwax_sub2db: a command line tool which is part of the mwax correlator for the MWA.\n" \
-                             "In CORRELATOR mode: Watches a directory and file pattern (for sub files). On new file, " \
-                             "launches diskdb to add the file to the designated PSRDADA ringbuffer.\n" \
-                             "In OFFLINE CORRELATOR mode: Retrieves a file list from a directory and file pattern. " \
-                             "For each file, launches diskdb to add the file to the designated PSRDADA ringbuffer.\n" \
-                             "In VOLTAGE_CAPTURE mode: Watches a directory and file pattern. On new file, " \
-                             "copy the sub file to local disk, where the archiver daemon will send to Pawsey/NGAS.\n" \
-                             "\n*.sub files are sub-observations of 8 seconds containing high time res data."
-        parser.add_argument("-w", "--watchdir", required=True, help="Directory to watch for *.sub files")
-        parser.add_argument("-k", "--key", required=True, help="PSRDADA ringbuffer key of the destination data block")
-        parser.add_argument("-d", "--dadadiskdbpath", required=True,
-                            help="Absolute path to PSRDADA dada_diskdb executable")
-        parser.add_argument("-m", "--mode", required=True, default=MODE_CORRELATOR,
-                            choices=[MODE_CORRELATOR, MODE_OFFLINE_CORRELATOR, MODE_VOLTAGE_CAPTURE, ],
-                            help=f"Mode to run- default={MODE_CORRELATOR}")
+        parser.description = "mwax_sub2db: a command line tool which is part of the mwax correlator for the MWA.\n"
+        parser.add_argument("-w", "--watchdir", required=True, help="Directory to watch for files with watchext "
+                                                                    "extension")
+        parser.add_argument("-x", "--watchext", required=True, help="Extension to watch for")
+        parser.add_argument("-r", "--renameext", required=True, help="Extension to rename file to")
+        parser.add_argument("-e", "--executablepath", required=True,
+                            help=f"Absolute path to executable to launch. {FILE_REPLACEMENT_TOKEN} will be substituted with the abs "
+                                  "path of the filename being processed.")
+        parser.add_argument("-m", "--mode", required=True, default=None,
+                            choices=[MODE_WATCH_DIR_FOR_NEW, MODE_WATCH_DIR_FOR_RENAME, MODE_PROCESS_DIR, ],
+                            help=f"Mode to run:\n"
+                            f"{MODE_WATCH_DIR_FOR_NEW}: Watch watchdir for new files forever. Launch executable. "
+                                 f"Rename file.\n" 
+                            f"{MODE_WATCH_DIR_FOR_RENAME}: Watch watchdir for renamed files forever. Launch executable. "
+                                 f"Rename file.\n" 
+                            f"{MODE_PROCESS_DIR}: For each file in watchdir, launch executable. Rename file. Exit.\n")
         args = vars(parser.parse_args())
 
         # Check args
         self.watch_dir = args["watchdir"]
-        self.dada_key = args["key"]
-        self.dada_diskdb = args["dadadiskdbpath"]
+        self.watch_ext = args["watchext"]
+        self.rename_ext = args["renameext"]
+        self.executable = args["executablepath"]
         self.mode = args["mode"]
 
-        if self.mode != MODE_CORRELATOR and self.mode != MODE_OFFLINE_CORRELATOR:
-            print(f"Error: --mode {self.mode} is not supported.")
-            exit(1)
-
         if not os.path.isdir(self.watch_dir):
-            print(f"Error: --watchdir {self.watch_dir} does not exist or you don't have permission.")
+            print(f"Error: --watchdir '{self.watch_dir}' does not exist or you don't have permission")
             exit(1)
 
-        if not os.path.isfile(self.dada_diskdb):
-            print(f"Error: --dadadiskdbpath {self.dada_diskdb} executable does not exist or you don't have permission.")
+        if not self.watch_ext[0] == ".":
+            print(f"Error: --watchext '{self.watch_ext}' should start with a '.' e.g. '.sub'")
+            exit(1)
+
+        if not self.rename_ext[0] == ".":
+            print(f"Error: --renameext '{self.rename_ext}' should start with a '.' e.g. '.done'")
             exit(1)
 
         # Create a queue for dealing with files
@@ -195,11 +224,11 @@ class Processor:
 
         # Create watcher
         self.watch = Watcher(path=self.watch_dir, recursive=False, q=self.q,
-                             pattern=f"*.{SUBFILE_EXTENSION}", log=self.logger)
+                             pattern=f"*.{self.watch_ext}", log=self.logger, mode=self.mode)
 
         # Create queueworker
-        self.queueworker = QueueWorker(q=self.q, dada_diskdb_path=self.dada_diskdb,
-                                       dada_key=self.dada_key, mode=self.mode, log=self.logger)
+        self.queueworker = QueueWorker(q=self.q, executable_path=self.executable,
+                                       mode=self.mode, log=self.logger, rename_ext=self.rename_ext)
 
         self.running = True
         self.logger.info("Processor Initialised...")
@@ -214,21 +243,23 @@ class Processor:
         # Make sure we can Ctrl-C / kill out of this
         signal.signal(signal.SIGINT, self.signal_handler)
 
-        self.logger.info(f"Running in {self.mode}")
+        self.logger.info(f"Running in mode {self.mode}")
 
-        if self.mode == MODE_CORRELATOR:
+        if self.mode == MODE_WATCH_DIR_FOR_RENAME or self.mode == MODE_WATCH_DIR_FOR_NEW:
+            self.logger.info(f"Scanning {self.watch_dir} for files matching {'*' + self.watch_ext}...")
+
             # Setup thread for watching filesystem
             watcher_thread = threading.Thread(target=self.watch.start, daemon=True)
 
             # Start watcher
             watcher_thread.start()
-        elif self.mode == MODE_OFFLINE_CORRELATOR:
+        elif self.mode == MODE_PROCESS_DIR:
             # we don't need a watcher
             watcher_thread = None
 
             # Just loop through all files and add them to the queue
-            pattern = os.path.join(self.watch_dir, "*" + SUBFILE_EXTENSION)
-            self.logger.info(f"Scanning {self.watch_dir} for files matching {'*' + SUBFILE_EXTENSION}...")
+            pattern = os.path.join(self.watch_dir, "*" + self.watch_ext)
+            self.logger.info(f"Scanning {self.watch_dir} for files matching {'*' + self.watch_ext}...")
 
             files = glob.glob(pattern)
 
--- test_process_dir.sh
+++ test_process_dir.sh
@@ -2,7 +2,7 @@
 echo ...instantiating input ring buffer...
 dada_db -b 26214400 -k 0x1234 -n 400 -p
 
-python mwax_sub2db.py -w /tmp/greg/ -k 0x1234 -d /usr/local/bin/dada_diskdb -m=CORRELATOR
+python mwax_sub2db.py -w /tmp/greg/ -x ".txt" -e "echo 'processing __FILE__'" -r ".done" -m PROCESS_DIR
 #dada_diskdb -k 0x1234 -f /tmp/greg/1216447872_02_256_201.sub -s
 
 echo ...destroying input ring buffer
