0dc367235fb958f1386780197777b7ccf47c88bc
Greg Sleap <greg.sleap@curtin.edu.au>
2022-09-27 14:09:22 +1000

    Fixed health status bug

--- src/mwax_mover/mwax_priority_queue_worker.py
+++ src/mwax_mover/mwax_priority_queue_worker.py
@@ -192,6 +192,6 @@ class PriorityQueueWorker(object):
         """Return the status as a dictionary"""
         return {
             "Unix timestamp": time.time(),
-            "current item": self.current_item,
+            "current item": str(self.current_item[1]),
             "priority_queue_size": self.source_queue.qsize(),
         }
