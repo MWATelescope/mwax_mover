6ae48c950d30502c84466cec1f1d71b16aef4d16
Greg Sleap <greg.sleap@curtin.edu.au>
2024-11-06 11:43:58 +1100

    Fixed type error with found_obsid variable

--- src/mwax_mover/mwax_calvin_processor.py
+++ src/mwax_mover/mwax_calvin_processor.py
@@ -17,6 +17,7 @@ import signal
 import sys
 import threading
 import time
+from typing import Optional
 from mwax_mover import (
     utils,
     version,
@@ -362,9 +363,9 @@ class MWAXCalvinProcessor:
             obs_id: int = int(filename[0:10])
 
             # Check to see if we are already processing this observation
-            found_obsid: CurrentObsID
+            found_obsid: Optional[CurrentObsID] = None
             if int(obs_id) in self.current_obsids:
-                found_obsid: CurrentObsID = self.current_obsids[int(obs_id)]
+                found_obsid = self.current_obsids[int(obs_id)]
 
             if found_obsid is not None and found_obsid.assembled:
                 # hmm so we just got a file for obsid xxx, yet xxx is already further along
